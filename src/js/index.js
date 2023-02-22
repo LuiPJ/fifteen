03004fd7c04ec1c889dc9bf9f5f63f3381c53e63
Nikita Kanaev <68945163+LuiPJ@users.noreply.github.com>
2023-02-22 11:03:38 +0300

    add js

--- src/js/index.js
+++ src/js/index.js
@@ -6,4 +6,37 @@ if (itemNodes.length !== 16) {
   throw new Error(`Должно быть ровно ${countitems}  items in HTML`);
 }
 
-let matrix = getMatrix(itemNodes);
+let matrix = getMatrix(itemNodes.map((item) => Number(item.dataset.matrixId)));
+setPositionItems(matrix);
+
+function getMatrix(arr) {
+  const matrix = [[], [], [], []];
+  //
+  let y = 0;
+  let x = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (x >= 4) {
+      y++;
+      x = 0;
+    }
+    matrix[y][x] = arr[i];
+    x++;
+  }
+  return matrix;
+}
+
+function setPositionItems(matrix) {
+  for (let y = 0; y < matrix.length; y++) {
+    for (let x = 0; x < matrix[y].length; x++) {
+      const value = matrix[y][x];
+      const node = itemNodes[value - 1];
+      setNodeStyles(node, x, y);
+    }
+  }
+}
+
+function setNodeStyles(node, x, y) {
+  const shiftPs = 100;
+  node.style.transform = `translate3D(${x * shiftPs}%, ${y * shiftPs}%,0)`;
+}
